cf9b65b97e0ce6af26f9dafb4f0e49718d43b508
Tarang Sachdev <43231377+tarangsachdev92@users.noreply.github.com>
2026-04-12 21:48:53 +0530

    Add factorial function implementation in 11.factorial.js

---
+++ 11.factorial.js
@@ -0,0 +1,12 @@
+const num = 10;
+// 5!; 5*4*3*2*1
+// n!; n * (n-1) * (n-2)
+
+function factorial(n) {
+    if (n == 1 || n == 0) {
+        return 1
+    }
+    return n * factorial(n - 1)
+}
+
+console.log(factorial(num))
