a10f86569bc4256bd2f3a0c8f37af45287acb983
Tarang Sachdev <43231377+tarangsachdev92@users.noreply.github.com>
2026-04-15 00:12:15 +0530

    Implement bubble sort algorithm in 12.bubble sort.js

---
+++ 12.bubble sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
+
+// Example
+console.log(bubbleSort([5, 3, 8, 4, 2]));
